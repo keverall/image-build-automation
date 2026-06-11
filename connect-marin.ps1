c9f26cec16336589e37d89915b5dcf0369f41079
Kev Everall <keverall@gmail.com>
2026-06-11 15:40:35 +0100

    Remove hardcoded username and IP from script
    Removed username and IP variable assignments from the script.

--- connect-marin.ps1
+++ connect-marin.ps1
@@ -1,6 +1,6 @@
 # OneView information
-$username = "adm_98253"
-# $IP = "va-oneviewt-01.ad.example.com"
+
+
 
 $secpasswd = read-host  "Please enter the OneView password" -AsSecureString
 # Connection to the Synergy Composer
